5de74cca5c0b7fb9f4a65374bb183951b81e6d33
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2015-12-27 10:24:00 -0200

    fetching data natively

--- app/src/jsx/components/topic-list.jsx
+++ app/src/jsx/components/topic-list.jsx
@@ -1,7 +1,34 @@
 var React = require('react');
+var Api = require('../utils/api');
 
 module.exports = React.createClass({
+	
+	getInitialState: function(){
+		return { topics: [] };
+	},
+
+	componentWillMount: function(){
+		Api.get('topics/defaults')
+		 .then(function(res){
+		 	this.setState({
+		 		topics: res.data
+		 	})
+		 }.bind(this));
+	},
+
+	renderTopics: function(){
+		return this.state.topics.map(function(topic){
+			return <li> {topic.description} </li>
+		});
+	},
+
 	render: function(){
-		return <h1>Topic List</h1>
+		return ( 
+			<div>
+				Topics List
+				{this.renderTopics()}
+			</div>
+		);
+
 	}
 });
