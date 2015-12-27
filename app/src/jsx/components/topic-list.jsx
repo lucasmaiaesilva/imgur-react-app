f1bf73c09ddcf8c5625c1d02be4aab5143691b48
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2015-12-27 11:50:32 -0200

    triggering changes from a store

--- app/src/jsx/components/topic-list.jsx
+++ app/src/jsx/components/topic-list.jsx
@@ -1,7 +1,16 @@
 var React 		= require('react');
+var Reflux 		= require('reflux');
 var TopicStore 	= require('../stores/topic-store');
 
 module.exports = React.createClass({
+
+	// this mixin say, this component that we'll writing right now needs to listen to
+	// any event that was trigger to topicStore 
+	// the handleChange is a function that send the data to the other function
+	// trigger on the topicStore component
+	mixins: [
+		Reflux.listenTo(TopicStore, 'handleChange')
+	],
 	
 	getInitialState: function(){
 		return { topics: [] };
@@ -9,21 +18,16 @@ module.exports = React.createClass({
 
 	componentWillMount: function(){
 		TopicStore.getTopics()
-			.then(function(){
-				// we have successfully fetched topics
-				// topics are available on TopicStore.topics
-				// after a promisse we know that topics are ok to use
-				this.setState({
-					topics: TopicStore.topics
-				});
-
-			}.bind(this));
 	},
 
 	renderTopics: function(){
 		return this.state.topics.map(function(topic){
 			return <li> {topic.description} </li>
 		});
+	},
+
+	handleChange: function(event, updatedTopics ){
+		this.setState({ topics: updatedTopics })
 	},
 
 	render: function(){
--- app/src/jsx/stores/topic-store.jsx
+++ app/src/jsx/stores/topic-store.jsx
@@ -6,6 +6,14 @@ module.exports = Reflux.createStore({
 		return Api.get('topics/defaults')
 			.then(function(res){
 				this.topics = res.data;
+				this.triggerChange();
 			}.bind(this));
+	},
+	triggerChange: function(){
+
+		this.trigger('change', this.topics);
+		// this.trigget is a function used by reflux to put togeter events and data
+		// the first parameter is a event that I want to use
+		// and the second parameter is the data itself
 	}
 });
