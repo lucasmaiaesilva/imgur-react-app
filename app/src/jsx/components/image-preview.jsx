501667f1bedf71a73f15375076d7bad06b789720
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2015-12-28 21:06:28 -0200

    playing videos onMouseOver

--- app/src/jsx/components/image-preview.jsx
+++ app/src/jsx/components/image-preview.jsx
@@ -1,14 +1,42 @@
 var React = require('react');
 
 module.exports = React.createClass({
+
+	getInitialState: function(){
+		return {hovering: false}
+	},
 	
+	handleMouseEnter: function(){
+		this.setState({ hovering: true });
+	},
+
+	handleMouseLeave: function(){
+		this.setState({ hovering: false });
+	},
+
 	image: function(){
-		console.log(this.props);
 		var link = 'http://i.imgur.com/' + this.props.image.id + 'h.jpg';
 		return <img src={link} />
 	},
 
+	video: function(){
+		return (
+			<div>
+				<video preload="auto" autoPlay="autoplay" loop="loop" webkit-playsinline>
+					<source src={this.props.image.mp4} type='video/mp4'></source>
+				</video>
+			</div>
+		);
+	},
+
 	render: function(){
-		return <div>{this.image()}</div>
+		return (
+			<div 
+			onMouseEnter={this.handleMouseEnter}
+			onMouseLeave={this.handleMouseLeave}
+			>
+				{this.props.image.animated && this.state.hovering ? this.video() : this.image()}
+			</div>
+		);
 	}
 });
