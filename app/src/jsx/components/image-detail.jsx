6a2bb59e33927b02a168a976c688def16f43b5fa
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2015-12-30 19:53:34 -0200

    rendering an image detail

--- app/src/jsx/components/image-detail.jsx
+++ app/src/jsx/components/image-detail.jsx
@@ -23,11 +23,38 @@ module.exports = React.createClass({
 		this.setState( {image: ImageStore.find(this.props.params.id)} );
 	},
 
+	renderContent: function() {
+		return (
+			<div>
+				<h4>{this.state.image.title}</h4>
+				<div>
+					{this.renderImage()}
+				</div>
+				<div>
+					<p>{this.state.image.description}</p>
+				</div>
+			</div>
+		);
+	},
+
+	renderImage: function() {
+		if(this.state.image.animated){
+			return (
+				<video preload="auto" autoPlay="autoPlay" loop="loop" webkit-playsinline>
+					<source src={this.state.image.mp4} type="video/mp4"></source>
+				</video>
+			);
+
+		} else {
+			return <img src={this.state.image.link} />
+		}
+	},
+
 	render: function(){
 		console.log(this.state.image);
 		return (
 			<div>
-				image page
+				{ this.state.image ? this.renderContent() : null }
 			</div>
 		);
 	}
